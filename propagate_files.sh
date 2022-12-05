a52ac4028838e31c6c63d830e57c780e196ab965
Simon Misencik <simon.misencik@gmail.com>
2022-12-05 20:36:36 +0100

    Fix condition
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- propagate_files.sh
+++ propagate_files.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 echo "source_enviroment: $1"
 echo "target_enviroment: $2"
@@ -6,7 +6,8 @@ echo "prefix_folder: $3"
 echo "suffix_folder: $4"
 echo "secrets_promotion: $5"
 
-if [ "$5" == "false" ]; then
+if [ "$5" == "false" ]
+then
   rsync -avc --del --exclude='*-secret.yaml' --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
 else
   rsync -avc --del --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
