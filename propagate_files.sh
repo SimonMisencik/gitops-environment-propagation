eb6cdbb18db60be2d64f4a333a4ed378c2744513
Simon Misencik <simon.misencik@gmail.com>
2022-12-05 20:39:33 +0100

    Fix command
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- propagate_files.sh
+++ propagate_files.sh
@@ -9,7 +9,7 @@ echo "secrets_promotion: $5"
 if [ "$5" == "false" ]
 then
   ls
-  rsync -avc --del --exclude='*-secret.yaml' --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
+  rsync -avc --del --exclude='*-secret.yaml' --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$2/$4
 else
   rsync -avc --del --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
 fi
