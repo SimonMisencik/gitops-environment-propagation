73f6f41fb0faf24344659a809fd7a9e9f184e2f1
Simon Misencik <simon.misencik@gmail.com>
2022-12-05 20:25:17 +0100

    Fix error in script
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- propagate_files.sh
+++ propagate_files.sh
@@ -6,7 +6,7 @@ echo "prefix_folder: $3"
 echo "suffix_folder: $4"
 echo "secrets_promotion: $5"
 
-if [ $5 == 'false']; then
+if [ $5 == 'false' ]; then
   rsync -avc --del --exclude='*-secret.yaml' --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
 else
   rsync -avc --del --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
