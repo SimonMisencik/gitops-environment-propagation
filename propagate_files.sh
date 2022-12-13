e552964daa39e1a37a7389e4cf9e398942287117
Šimon Mišenčík <simon.misencik@gmail.com>
2022-12-13 15:51:52 +0100

    Fix bug in promote secret

--- propagate_files.sh
+++ propagate_files.sh
@@ -8,7 +8,7 @@ echo "secrets_promotion: $5"
 
 if [ "$5" == "false" ]
 then
-  rsync -avc --del --exclude='*-secret.yaml' --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$2/$4
+  rsync -avc --del --exclude='*-secret.yaml' --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='development-values.yaml' $3/$1/$4/ $3/$2/$4
 else
-  rsync -avc --del --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='develop-values.yaml' $3/$1/$4/ $3/$3/$4
+  rsync -avc --del --exclude="$1-values.yaml" --exclude="$2-values.yaml" --exclude='development-values.yaml' $3/$1/$4/ $3/$2/$4
 fi
